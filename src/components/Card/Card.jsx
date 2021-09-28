b8662dede00557875fd2fce55db11decd7616c5d
Your Name <tolkun.kolya@mail.ru>
2021-09-28 05:00:14 +0300

    feat: add Card, Dashboard, Header, Button, Input

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import css from "./style.module.css"
+export { Button, Input } from "../common/"
+
+
+export class Card extends React.Component {
+    render() {
+        return(
+            <div className={css.card}>
+                <div>
+                    <div>
+                        { title }
+                    </div>
+                    <div>
+                        <Input />
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <Button>
+                            zdgsdg
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
